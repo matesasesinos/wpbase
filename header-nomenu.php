3656594e03313a34453b2b0d4cd89a4ed8349fd0
matezito <juan.e@genosha.com.ar>
2020-06-29 10:33:41 -0300

    fix

---
+++ header-nomenu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="h-100">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
---
+++ page-login.php
@@ -0,0 +1,5 @@
+<?php 
+/* Template Name: Login */
+get_header('nomenu'); ?>
+    <?php get_template_part('parts/page', 'full'); ?>
+<?php get_footer('nomenu'); ?>
